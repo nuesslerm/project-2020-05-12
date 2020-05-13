1110f7e9cc960868fd8a5da9c5d51feacab85a30
nuesslerm <markus@choco.com>
2020-05-13 02:25:05 +0200

    done with update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
+const HomePage = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>HOME PAGE</h1>
     </div>
   );
+};
+
+const TopicList = () => {
+  return (
+    <div>
+      <h1>TOPIC LIST PAGE</h1>
+    </div>
+  );
+};
+
+const TopicDetail = () => {
+  return (
+    <div>
+      <h1>TOPIC DETAIL PAGE</h1>
+    </div>
+  );
+};
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Hello World</p>
+      </div>
+    );
+  }
 }
 
 export default App;
